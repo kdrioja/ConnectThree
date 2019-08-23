8b94aa99bd621d094088ac35b0864d44c85dc3d5
Kenia Rioja <kdrioja@gmail.com>
2019-08-23 14:30:13 -0700

    Game is playable. Need to work on identifying when a player has won.

--- app/src/main/java/com/example/connectthree/MainActivity.java
+++ app/src/main/java/com/example/connectthree/MainActivity.java
@@ -8,6 +8,12 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //could use a boolean but the int has the advantage of allowing more players
+    //to be added in the future
+    // 0: yellow
+    // 1: red
+    int activePlayer = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,15 @@ public class MainActivity extends AppCompatActivity {
         ImageView counter = (ImageView) view;
 
         counter.setTranslationY(-1500);
-        counter.setImageResource(R.drawable.yellow);
+
+        if (activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow);
+            activePlayer = 1;
+        }
+        else {
+            counter.setImageResource(R.drawable.red);
+            activePlayer = 0;
+        }
 
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
     }
