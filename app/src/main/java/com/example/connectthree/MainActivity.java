775ab9b06042b8da696a2809860c523be89710a2
Kenia Rioja <kdrioja@gmail.com>
2019-08-23 14:11:01 -0700

    Writing dropin function

--- app/src/main/java/com/example/connectthree/MainActivity.java
+++ app/src/main/java/com/example/connectthree/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.connectthree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropin(View view) {
+        ImageView counter = (ImageView) view;
+
+        counter.setTranslationX(-1500);
+        counter.setImageResource(R.drawable.yellow);
+
+        counter.animate().translationYBy(1500).setDuration(300);
     }
 }
